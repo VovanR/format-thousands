ffe2dcaec2811a61fab0e9b8d0007c1270f5ca75
Vladimir Rodkin <mail@vovanr.com>
2020-12-30 12:27:16 +0300

    build(package): Upgrade development dependencies
    Migrate from tabs to 2 spaces;
    
    BREAKING CHANGE: Drop support of Node.js < v10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -31,6 +31,3 @@
 <a name="1.0.3"></a>
 ## [1.0.3](https://github.com/vovanr/format-thousands/compare/v1.0.2...v1.0.3) (2016-04-21)
 
-
-
-
--- index.js
+++ index.js
@@ -2,14 +2,13 @@
  * Non-Breaking Space `&nbsp;`
  * @const {string}
  */
-var NBSP = String.fromCharCode(160);
+const NBSP = String.fromCharCode(160);
 
 /**
- * @typedef ParsedNumber
- * @prop {Object} options
- * @prop {string} options.integer
- * @prop {string} options.fraction
- * @prop {string} options.sign
+ * @typedef {Object} ParsedNumber
+ * @prop {string} integer
+ * @prop {string} fraction
+ * @prop {string} sign
  */
 
 /**
@@ -17,17 +16,20 @@ var NBSP = String.fromCharCode(160);
  * @returns {ParsedNumber}
  */
 function parseNumber(number) {
-	var isNegative = number < 0;
-	var numberString = String(number);
-	if (isNegative) {
-		numberString = numberString.slice(1);
-	}
-	var decimal = numberString.split('.');
-	return {
-		integer: decimal[0],
-		fraction: decimal[1] || '',
-		sign: isNegative ? '-' : ''
-	};
+  const isNegative = number < 0;
+  let numberString = String(number);
+
+  if (isNegative) {
+    numberString = numberString.slice(1);
+  }
+
+  const decimal = numberString.split('.');
+
+  return {
+    integer: decimal[0],
+    fraction: decimal[1] || '',
+    sign: isNegative ? '-' : ''
+  };
 }
 
 /**
@@ -36,27 +38,28 @@ function parseNumber(number) {
  * @returns {string}
  */
 function format(number, separator) {
-	number = String(number);
+  number = String(number);
 
-	while (number.length % 3) {
-		number = '#' + number;
-	}
+  while (number.length % 3) {
+    number = '#' + number;
+  }
 
-	var result = number.substr(0, 3);
-	result = result.replace(/#/g, '');
-	var i;
-	var length = number.length;
-	for (i = 3; i < length; i += 3) {
-		result = result + separator + number.substr(i, 3);
-	}
+  let result = number.substr(0, 3);
+  result = result.replace(/#/g, '');
 
-	return result;
+  let i;
+  const {length} = number;
+  for (i = 3; i < length; i += 3) {
+    result = result + separator + number.substr(i, 3);
+  }
+
+  return result;
 }
 
 /**
  * @param {number} number
- * @param {Object|string} [options=' ']
- * @param {string} [options.separator=' ']
+ * @param {Object|string} [options=" "]
+ * @param {string} [options.separator=" "]
  * @param {boolean} [options.formatFourDigits=true]
  * @returns {string}
  *
@@ -71,42 +74,42 @@ function format(number, separator) {
  * //=> "10'000"
  */
 module.exports = function (number, options) {
-	var result = '';
-	var separator = NBSP;
-	var formatFourDigits = true;
+  let result = '';
+  let separator = NBSP;
+  let formatFourDigits = true;
 
-	if (!number && number !== 0) {
-		return result;
-	}
+  if (!number && number !== 0) {
+    return result;
+  }
 
-	var numberObject = parseNumber(number);
-	var numberString = String(number);
+  const numberObject = parseNumber(number);
+  const numberString = String(number);
 
-	if (typeof options === 'object') {
-		if (options.separator) {
-			separator = options.separator;
-		}
+  if (typeof options === 'object') {
+    if (options.separator) {
+      ({separator} = options);
+    }
 
-		if (typeof options.formatFourDigits === 'boolean') {
-			formatFourDigits = options.formatFourDigits;
-		}
-	} else if (typeof options !== 'undefined') {
-		separator = options;
-	}
+    if (typeof options.formatFourDigits === 'boolean') {
+      ({formatFourDigits} = options);
+    }
+  } else if (typeof options !== 'undefined') {
+    separator = options;
+  }
 
-	if (
-		numberObject.integer.length <= 3 ||
-		(numberObject.integer.length === 4 && !formatFourDigits)
-	) {
-		result = numberString;
-	} else {
-		result += numberObject.sign;
-		result += format(numberObject.integer, separator);
-		if (numberObject.fraction) {
-			result += '.';
-			result += numberObject.fraction;
-		}
-	}
+  if (
+    numberObject.integer.length <= 3 ||
+    (numberObject.integer.length === 4 && !formatFourDigits)
+  ) {
+    result = numberString;
+  } else {
+    result += numberObject.sign;
+    result += format(numberObject.integer, separator);
+    if (numberObject.fraction) {
+      result += '.';
+      result += numberObject.fraction;
+    }
+  }
 
-	return result;
+  return result;
 };
